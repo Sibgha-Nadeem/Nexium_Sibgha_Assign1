9baa9e9da332345eefb00190c4e51a8fdf48c5c5
Sibgha N <l226617@lhr.nu.edu.pk>
2025-07-03 13:26:08 +0500

    Create quotes.ts

---
+++ quotes.ts
@@ -0,0 +1,8 @@
+ export const quotes = [
+  { topic: "inspiration", text: "Believe you can and you're halfway there." },
+  { topic: "inspiration", text: "Your time is limited, don’t waste it." },
+  { topic: "inspiration", text: "Dream big and dare to fail." },
+  { topic: "life", text: "Life is what happens when you’re busy making other plans." },
+  { topic: "life", text: "Get busy living or get busy dying." },
+  { topic: "life", text: "The purpose of our lives is to be happy." }
+];
