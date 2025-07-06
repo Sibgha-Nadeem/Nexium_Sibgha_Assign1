cdbac1989b262a409c14d0ce01953f4091092fba
Sibgha N <l226617@lhr.nu.edu.pk>
2025-07-06 13:49:22 +0500

    Add files via upload

--- quotes.ts
+++ quotes.ts
@@ -4,5 +4,8 @@
   { topic: "inspiration", text: "Dream big and dare to fail." },
   { topic: "life", text: "Life is what happens when you’re busy making other plans." },
   { topic: "life", text: "Get busy living or get busy dying." },
-  { topic: "life", text: "The purpose of our lives is to be happy." }
+  { topic: "life", text: "The purpose of our lives is to be happy." },
+  { topic: "sad", text: "There is no greater sorrow than to recall happiness in times of misery."},
+  { topic: "sad", text: "He who has felt the deepest grief is best able to experience supreme happiness."},
+  { topic: "sad", text: "Tears are the silent language of grief."}
 ];
